24f2d707c430b51fa73a5b5f512b6822f9551a25
Emerson Luiz <eluizbr@gmail.com>
2017-11-30 09:31:31 -0200

    Updates

--- src/config/config-dev.ts
+++ src/config/config-dev.ts
@@ -1,5 +1,5 @@
 export = {
-  mongoDB: 'mongodb://ecobonuz:eco2017bonuz@ds157624.mlab.com:57624/moeda',
-  jwtSecret: '9V2ek8KEjUB814ewYKqa38Td9XK15rNJraBJAhvasH5WtV6hSp',
+  mongoDB: '',
+  jwtSecret: '',
   modalTimer: 5000
 };
